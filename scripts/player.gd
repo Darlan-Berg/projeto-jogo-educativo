ecfd45744d1dc1d100dadbb8e64709be085d5f15
Darlan Berg <darlanbergsilva@gmail.com>
2024-11-07 15:36:11 -0300

    Ajustes nos inimigos  e no dano

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,29 @@
 extends CharacterBody2D
 
-
 const SPEED = 100.0
 const JUMP_VELOCITY = -300.0
 var jumpCount = 0
 var caiu_na_agua_1 = false
 var caiu_na_agua_2 = false
+var knockback_vector:= Vector2.ZERO
 
 @onready var animador := $anim as AnimatedSprite2D
 @onready var remote := $remote as RemoteTransform2D
 @onready var som_pulo := $som_pulo as AudioStreamPlayer
-
+@onready var ray_direita: RayCast2D = $ray_direita
+@onready var ray_esquerda: RayCast2D = $ray_esquerda
 
 func _physics_process(delta: float) -> void:
 	
+	if ray_direita.is_colliding():
+		Global.vida -= 1
+		tomar_dano(Vector2(-200, -200))
+	if ray_esquerda.is_colliding():
+		Global.vida -= 1
+		tomar_dano(Vector2(200, -200))
+		
+	
 	#Verifica a quantidade de vida
-	'''
 	if Global.vida == 3:
 		$"../HUD/control/container/container_barra_vida/vida_3".visible = true
 		$"../HUD/control/container/container_barra_vida/vida_2".visible = false
@@ -36,7 +44,6 @@ func _physics_process(delta: float) -> void:
 		$"../HUD/control/container/container_barra_vida/vida_2".visible = false
 		$"../HUD/control/container/container_barra_vida/vida_1".visible = false
 		$"../HUD/control/container/container_barra_vida/vida_0".visible = true
-	'''
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -70,6 +77,9 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if is_on_floor():
 			animador.play("idle")
+			
+	if knockback_vector != Vector2.ZERO:
+		velocity = knockback_vector 
 
 	move_and_slide()
 	
@@ -84,7 +94,13 @@ func _on_agua_2_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index
 	Global.vida -= 1
 	position.x = 1970
 	position.y = 107
+	
+func tomar_dano(forca_knockback := Vector2.ZERO, duracao := 0.25): 
+	
+	if forca_knockback != Vector2.ZERO: 
+		knockback_vector = forca_knockback 
 
-
-func _on_hurtbox_body_entered(body: Node2D) -> void:
-	print(body.name)
+		var knockback_tween := get_tree().create_tween() 
+		knockback_tween.tween_property(self, "knockback_vector", Vector2.ZERO, duracao)
+		animador.modulate = Color(1,0,0,1) 
+		knockback_tween.tween_property(animador, "modulate", Color(1,1,1,1), duracao)
