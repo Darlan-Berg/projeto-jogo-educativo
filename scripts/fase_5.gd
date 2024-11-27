a906bd297ab0348ac3ebea693c2e3addb25a1e0a
Darlan-Berg <darlanbergsilva@gmail.com>
2024-11-26 22:02:17 -0300

    Protótipo do esquema de mostrar a foto no final da fase

---
+++ scripts/fase_5.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+# quando o jogador chegar no fim da fase
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("res://cenas/tela_inicial.tscn")
--- scripts/tela_inicial.gd
+++ scripts/tela_inicial.gd
@@ -1,6 +1,10 @@
 extends CanvasLayer
 
 
+func _ready() -> void:
+	print("O valor de Global.pedacos_foto é ", Global.pedacos_foto)
+
+
 func _on_button1_pressed() -> void:
 	get_tree().change_scene_to_file("res://cenas/texto_cutscene_5.tscn")
 
