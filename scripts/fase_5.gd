1e21405fd4e19717d718ed5d1f4c850639b1152d
Darlan-Berg <darlanbergsilva@gmail.com>
2024-11-28 18:26:28 -0300

    Encerramento da fase 5

--- scripts/fase_5.gd
+++ scripts/fase_5.gd
@@ -3,4 +3,4 @@ extends Node2D
 
 # quando o jogador chegar no fim da fase
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://cenas/fim_fase_5.tscn")
+	get_tree().change_scene_to_file("res://cenas/final da fase 5/cena1.tscn")
